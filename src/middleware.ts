56ff9eeb4dc9f54648569778f782bc0962593ec0
Achmad Fatoni <achmad.fatoni@spesolution.com>
2026-04-10 11:57:50 +0700

    feat(middleware): redirect ADMIN to dashboard automatically when accessing home route

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,18 @@
 import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
 
-export default withAuth({
-  pages: {
-    signIn: "/auth/login",
+export default withAuth(
+  function middleware(req) {
+    if (req.nextUrl.pathname === "/" && req.nextauth.token?.role === "ADMIN") {
+      return NextResponse.redirect(new URL("/admin/stats", req.url));
+    }
   },
-});
+  {
+    pages: {
+      signIn: "/auth/login",
+    },
+  }
+);
 
 export const config = {
   matcher: [
